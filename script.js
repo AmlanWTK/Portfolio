026127421229982121710316f775d6b4a86ea4a2
AmlanSarkar <amlansarkar738@gmail.com>
2024-01-27 00:39:32 -0800

    first commit for lab1

---
+++ script.js
@@ -0,0 +1,15 @@
+
+var typed = new Typed(".auto-type", {
+  strings: [
+    "Developer", 
+    "Designer", 
+    "Photographer"
+  ],
+  typeSpeed: 100,
+  backSpeed: 60, // Set a higher backSpeed
+   // Set a small or zero startDelay
+  loop: true
+  
+  
+});
+
